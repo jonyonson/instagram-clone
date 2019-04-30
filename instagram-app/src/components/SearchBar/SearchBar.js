0315b792aeb3fe91376636e142e5a4f8d617fe77
Jonathan Taylor <jonyonson@icloud.com>
2019-04-30 14:31:38 -0500

    updated search bar

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import './SearchBar.css';
 
-const SearchBar = () => <div>SearchBar</div>;
+import logo from '../../images/logo.png';
+import activity from '../../images/activity.png';
+import explore from '../../images/explore.png';
+import profile from '../../images/profile.png';
+
+const SearchBar = () => {
+  return (
+    <div className="SearchBar__wrapper">
+      <div className="SearchBar">
+        <img className="SearchBar__logo" src={logo} alt="" />
+        <input
+          className="SearchBar__input"
+          type="text"
+          autoCapitalize="none"
+          placeholder="Search"
+        />
+        <div className="SearchBar__buttons">
+          <img src={activity} alt="" />
+          <img src={explore} alt="" />
+          <img src={profile} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
