f88f0b6331d4985a3e52a07b55a24d93809a8825
Jonathan Taylor <jonyonson@icloud.com>
2019-04-30 14:27:19 -0500

    finished top section of PostContainer (header and image)

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import CommentSection from '../CommentSection/CommentSection';
 
-const PostContainer = () => <div>PostContainer</div>;
+import './PostContainer.css';
 
+import dummyData from '../../data/dummy-data1';
+
+const PostContainer = () => {
+  return (
+    <article className="PostContainer">
+      <header className="PostContainer__header">
+        <div className="PostContainer__header__thumbnail">
+          <img src={dummyData[0].thumbnailUrl} alt="" />
+        </div>
+        <div className="PostContainer__header__username">
+          {dummyData[0].username}
+        </div>
+      </header>
+      <div className="PostContainer__image">
+        <img src={dummyData[0].imageUrl} alt="" />
+      </div>
+      <CommentSection />
+    </article>
+  );
+};
 export default PostContainer;
