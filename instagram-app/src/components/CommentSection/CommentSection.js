77821b8a2f2e88a93f9335e2f0b91a008e4adb19
Jonathan Taylor <jonyonson@icloud.com>
2019-05-02 18:40:02 -0500

    cleaned up code

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import TextareaAutosize from 'react-autosize-textarea';
+// import TextareaAutosize from 'react-autosize-textarea';
 import moment from 'moment';
 import styled from 'styled-components';
 
